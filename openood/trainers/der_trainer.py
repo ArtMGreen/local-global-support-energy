0406faf8f89f8f3865cc7c83d6caecd092c345c4
ArtMGreen <artyom5145@gmail.com>
2026-07-08 17:16:37 +0300

    DER hyperparams

--- openood/trainers/der_trainer.py
+++ openood/trainers/der_trainer.py
@@ -28,7 +28,8 @@ class DERTrainer:
         self.pixel_min = torch.tensor([(0 - m) / s for m, s in zip(self.input_mean, self.input_std)])
         self.pixel_max = torch.tensor([(1 - m) / s for m, s in zip(self.input_mean, self.input_std)])
 
-        self.adversary = FGSM(8/255, F.cross_entropy, self.pixel_min, self.pixel_max)
+        # self.adversary = FGSM(8/255, F.cross_entropy, self.pixel_min, self.pixel_max)
+        self.adversary = PGD(8/255, 1/255, 12, True, F.cross_entropy, self.pixel_min, self.pixel_max)
 
         self.optimizer = torch.optim.SGD(
             net.parameters(),
@@ -115,13 +116,14 @@ class DERTrainer:
 
 
 class FGSM:
-    def __init__(self, alpha, cls_loss_fn, pixel_min, pixel_max):
-        self.alpha = alpha
+    def __init__(self, eps, cls_loss_fn, pixel_min, pixel_max):
+        self.eps = eps
         self.cls_loss_fn = cls_loss_fn
         self.pixel_min = pixel_min
         self.pixel_max = pixel_max
 
     def attack(self, model, x, y):
+        # note: it is an untargeted one!
         was_training = model.training
         model.eval()
     
@@ -137,10 +139,61 @@ class FGSM:
     
         ranges = max_vals - min_vals
     
-        x_adv = x_orig + self.alpha * ranges * dL_dx.sign()
+        x_adv = x_orig + self.eps * ranges * dL_dx.sign()
         x_adv = torch.clamp(x_adv, min_vals, max_vals)
     
         if was_training:
             model.train()
     
         return x_adv.detach()
+
+    def description(self):
+        return f"FGSM(ε={(self.eps):.5f}≈{(self.eps * 255):.1f}/255)"
+
+
+class PGD:
+    def __init__(self, eps, step_size, steps, random_start, cls_loss_fn, pixel_min, pixel_max):
+        self.eps = eps    # ultimate boundary
+        self.step_size = step_size
+        self.steps = steps
+        self.random_start = random_start
+        self.cls_loss_fn = cls_loss_fn
+        self.pixel_min = pixel_min
+        self.pixel_max = pixel_max
+
+    def attack(self, model, x, y):
+        was_training = model.training
+        model.eval()
+
+        min_vals = self.pixel_min.to(x.device).view(1, 3, 1, 1).expand_as(x)
+        max_vals = self.pixel_max.to(x.device).view(1, 3, 1, 1).expand_as(x)
+    
+        ranges = max_vals - min_vals
+
+        min_vals_eps = torch.clamp(x - self.eps * ranges, min_vals, max_vals)
+        max_vals_eps = torch.clamp(x + self.eps * ranges, min_vals, max_vals)
+    
+        x_adv = x.detach().clone()
+
+        if self.random_start:
+            uniform01 = torch.rand(x_adv.size(), device=x_adv.device)
+            random_start = (2 * uniform01 - 1) * ranges * self.eps
+            x_adv += random_start
+            # usually is not supposed to do anything, given the construction of the random start
+            x_adv = torch.clamp(x_adv, min_vals_eps, max_vals_eps)
+
+        for i in range(self.steps):
+            x_adv.requires_grad_(True)
+            logits = model(x_adv)
+            loss = self.cls_loss_fn(logits, y)
+            dL_dx = torch.autograd.grad(loss, x_adv)[0]
+            x_adv = x_adv + self.step_size * ranges * dL_dx.sign()
+            x_adv = torch.clamp(x_adv, min_vals_eps, max_vals_eps).detach()
+
+        if was_training:
+            model.train()
+    
+        return x_adv.detach()
+
+    def description(self):
+        return f"PGD-{self.steps}(ε={(self.eps):.5f}≈{(self.eps * 255):.1f}/255), step_size={(self.step_size):.5f}≈{(self.step_size * 255):.1f}/255, random_start={self.random_start}"
