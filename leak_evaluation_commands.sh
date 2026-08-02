2adfbbb25f1684a7637228cabed3fd18519bc497
ArtMGreen <artyom5145@gmail.com>
2026-08-02 10:23:10 +0300

    ASCOOD eval

--- leak_evaluation_commands.sh
+++ leak_evaluation_commands.sh
@@ -13,3 +13,11 @@ python main.py --config configs/datasets/cifar10/cifar10.yml configs/datasets/ci
 # T2FNorm
 
 python main.py --config configs/datasets/cifar10/cifar10.yml configs/datasets/cifar10/cifar10_ood.yml configs/networks/t2fnorm_net.yml configs/pipelines/test/test_leak.yml configs/preprocessors/base_preprocessor.yml configs/postprocessors/leak.yml --network.checkpoint 'results/cifar10_t2fnorm_net_t2fnorm_e100_lr0.1_alpha0.1_default/s11033/best.ckpt' --mark=t2fnorm
+
+# DER
+
+python main.py --config configs/datasets/cifar10/cifar10.yml configs/datasets/cifar10/cifar10_ood.yml configs/networks/resnet18_32x32.yml configs/pipelines/test/test_leak.yml configs/preprocessors/base_preprocessor.yml configs/postprocessors/leak.yml --network.checkpoint 'results/cifar10_resnet18_32x32_der_e100_lr0.005_gamma0.2_beta0.5_clean_loss_included/s0/best.ckpt' --mark=der
+
+# ASCOOD
+
+python main.py --config configs/datasets/cifar10/cifar10.yml configs/datasets/cifar10/cifar10_ood.yml configs/networks/ascood_net.yml configs/pipelines/test/test_leak.yml configs/preprocessors/base_preprocessor.yml configs/postprocessors/leak.yml --network.checkpoint 'results/cifar10_ascood_net_ascood_e100_lr0.1_w1.0_p0.15_otype_shuffle_alpha_10.0_10.0_kl_div_True_default/s0/best.ckpt' --mark=ascood
--- openood/networks/ascood_net.py
+++ openood/networks/ascood_net.py
@@ -19,8 +19,23 @@ class ASCOODNet(nn.Module):
     def set_params(self, sigma):
         self.sigma.fill_(sigma)
 
-    def forward(self, x, return_feature=False):
-        _, feature = self.backbone(x, return_feature=True)
+    def forward(self, x, return_feature=False, return_with_layers_left=False):
+        if return_with_layers_left:
+            _, feature_to_return = self.backbone(x, return_with_layers_left=return_with_layers_left)
+            _, feature = self.backbone.continue_forward(feature_to_return, layers_left=return_with_layers_left, return_feature=True)
+        else:
+            _, feature = self.backbone(x, return_feature=True)
+        pre_feature = feature.clone()
+        feature = z_std(feature) * self.sigma
+        output = self.get_fc_layer()(feature)
+        if return_with_layers_left:
+            return output, feature_to_return
+        if return_feature:
+            return output, pre_feature
+        return output
+
+    def continue_forward(self, feature, layers_left, return_feature=False):
+        _, feature = self.backbone.continue_forward(feature, layers_left=layers_left, return_feature=True)
         pre_feature = feature.clone()
         feature = z_std(feature) * self.sigma
         output = self.get_fc_layer()(feature)
